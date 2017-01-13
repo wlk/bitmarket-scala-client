2803db26c04f6788363e1671c36227ce902af9e6
Wojciech Langiewicz <wlangiewicz@gmail.com>
2017-01-13 22:31:57 +0100

    api cleanup

--- src/main/scala/com/wlangiewicz/bitmarket/Client.scala
+++ src/main/scala/com/wlangiewicz/bitmarket/Client.scala
@@ -2,7 +2,7 @@ package com.wlangiewicz.bitmarket
 
 import akka.actor.ActorSystem
 
-import scala.concurrent.Await
+import scala.concurrent.{ExecutionContext, Future}
 import scala.language.postfixOps
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.Http
@@ -10,37 +10,66 @@ import akka.http.scaladsl.model.headers.RawHeader
 import akka.http.scaladsl.unmarshalling.Unmarshal
 import akka.stream.ActorMaterializer
 
-import scala.concurrent.duration._
 import collection.immutable.Seq
 import com.roundeights.hasher.Implicits._
 import spray.json._
-import scala.concurrent.ExecutionContext.Implicits.global
 
 class Client(publicApiKey: String, privateApiKey: String)(implicit system: ActorSystem, materializer: ActorMaterializer) {
   val HeaderApiKey = "API-Key"
   val HeaderSignature = "API-Hash"
-
   val BaseUrl = "https://www.bitmarket.pl/api2/"
 
-  val uri = Uri().withQuery(Uri.Query.apply(Map("method" -> "info", "tonce" -> (System.currentTimeMillis / 1000).toString)))
+  private def requestBody(method: String): String = {
+    Uri()
+      .withQuery(
+        Uri.Query.apply(
+          Map(
+            "method" -> method,
+            "tonce" -> (System.currentTimeMillis / 1000).toString
+          )
+        )
+      )
+      .rawQueryString
+      .getOrElse("")
+  }
 
-  val body = uri.rawQueryString.getOrElse("")
-  println(body)
+  private def httpRequest(signedBody: String, body: String) = {
+    HttpRequest(
+      uri = BaseUrl, method = HttpMethods.POST,
+      headers = Seq(RawHeader(HeaderApiKey, publicApiKey), RawHeader(HeaderSignature, signedBody)),
+      entity = HttpEntity(body)
+    )
+  }
 
-  val sign = body.hmac(privateApiKey).sha512.hex
+  /**
+   * Generates a HttpRequest that can be later performed to get Info response
+   * @see https://github.com/bitmarket-net/api#info---account-information
+   * @return HttpRequest performing Info request
+   */
+  def infoRequest: HttpRequest = {
+    val body = requestBody(method = "info")
+    val signedBody = body.hmac(privateApiKey).sha512.hex
 
-  val request = HttpRequest(
-    uri = BaseUrl, method = HttpMethods.POST,
-    headers = Seq(RawHeader(HeaderApiKey, publicApiKey), RawHeader(HeaderSignature, sign)),
-    entity = HttpEntity(body)
-  )
+    httpRequest(signedBody, body)
+  }
 
-  println(s"request: $request")
+  /**
+   * Performs a single HttpRequest
+   * @param request
+   * @return Future[HttpResponse]
+   */
+  def performRequest(request: HttpRequest): Future[HttpResponse] = {
+    Http().singleRequest(request)
+  }
 
-  val response = Await.result(Http().singleRequest(request), 5 seconds)
-
-  val result = Await.result(Unmarshal(response).to[String].map(_.parseJson), 10 seconds)
-
-  println(result)
+  /**
+   * Parses HttpResponse into objects
+   * @param httpResponse A HttpResponse that contains response from the Bitmarket API
+   * @param executionContext Required by underlying API
+   * @return Unmarshaled response as JsValue
+   */
+  def unmarshalResponse(httpResponse: HttpResponse)(implicit executionContext: ExecutionContext): Future[JsValue] = {
+    Unmarshal(httpResponse).to[String].map(_.parseJson)
+  }
 }
 
